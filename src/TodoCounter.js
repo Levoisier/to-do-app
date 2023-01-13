de89504902744d4c30e5fcd32f884aa4f25cd0cd
Cristian Zapata <c.zapata.iq@gmail.com>
2023-01-12 22:13:16 -0500

    first styles added

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './TodoCounter.css';
+
+function TodoCounter() {
+    return (
+        <h2 className="TodoCounter"> You've completed 2 of 3 tasks</h2>  
+    );
+}
+
+export {TodoCounter};
+
